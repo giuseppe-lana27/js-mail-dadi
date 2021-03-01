7b8aab97b5894d9cd2a8f9443496106e1aaf230e
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-01 15:42:20 +0100

    inserito calcolo vincitore gioco dei dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -5,3 +5,10 @@ document.getElementById('numero-utente').innerHTML = numeroUtente;
 var numeroComputer = Math.ceil((Math.random() * 5) + 1);
 document.getElementById('numero-computer').innerHTML = numeroComputer;
 // 3. confronto i due numeri
+if (numeroUtente > numeroComputer){
+  document.getElementById('vincitore').innerHTML = "Hai Vinto!";
+} else if (numeroUtente == numeroComputer) {
+  document.getElementById('vincitore').innerHTML = "Siete pari!";
+} else if (numeroUtente < numeroComputer) {
+  document.getElementById('vincitore').innerHTML = "Hai Perso!";
+}
